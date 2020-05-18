45edb2f0ee56a54a0bd461be361b5dac92d8dd9b
suryangkoro <angkorosurya20@gmail.com>
2020-05-18 11:10:34 +0700

    first commit

---
+++ src/projecttest/ProjectTest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projecttest;
+
+/**
+ *
+ * @author l7systems
+ */
+public class ProjectTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
